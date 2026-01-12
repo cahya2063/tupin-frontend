79e22a774ec9c536d0f992a4f610018966f9f31c
Muhamad Cahya Yunizar <cronosstar007@gmail.com>
2026-01-12 21:12:07 +0700

    update

--- src/pages/transaction/paymentHistory.vue
+++ src/pages/transaction/paymentHistory.vue
@@ -149,7 +149,6 @@ onMounted(async () => {
           </v-btn>
         </template> -->
 
-
         <v-card
           prepend-icon="ri-delete-bin-line"
           text="Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running."
