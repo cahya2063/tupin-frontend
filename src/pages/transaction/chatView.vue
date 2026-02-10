f7c7790fd65d72ad9c5e3cf9a7200e21b9f8e0c0
Muhamad Cahya Yunizar <cronosstar007@gmail.com>
2026-02-10 23:12:26 +0700

    update

--- src/pages/transaction/chatView.vue
+++ src/pages/transaction/chatView.vue
@@ -132,7 +132,7 @@ async function shareLocation() {
       createdAt: new Date(),
     }
     
-    //test
+    //test 2
 
     console.log('data lokasi : ', msg)
     await apiFetch(`/messages/send`, {
